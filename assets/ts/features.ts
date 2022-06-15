9fed5c49a886dcdf05f4847c16f36100b8af30d7
Kristoffer Balintona <krisbalintona@gmail.com>
2022-06-15 13:57:28 -0500

    Never use side footnotes when the window is not wide enough

--- assets/ts/features.ts
+++ assets/ts/features.ts
@@ -1,22 +1,32 @@
-import ThemeColorScheme from "ts/colorScheme"
-import { renderCopyButton } from "ts/copyButton"
-import { renderFootnotes } from "ts/footnotes"
+import ThemeColorScheme from "ts/colorScheme";
+import { renderCopyButton } from "ts/copyButton";
+import { renderFootnotes } from "ts/footnotes";
 
-let enableFootnotes = false
+let enableFootnotes = false;
+let paramValue = document.currentScript.dataset.enableFootnotes == "true"; // Hugo parameter value
+
+function sideFootnotes() {
+  let appropriateWindowSize = window.innerWidth >= 865 + 270 + 100; // Window width
+  return (enableFootnotes = paramValue && appropriateWindowSize);
+}
+
+// FIXME 2022-06-15: Doesn't go to normal footnotes on window resize once side footnotes have been rendered initially. See renderFootnotes in footnotes.ts
+function showFootnotes() {
+  new ThemeColorScheme(document.getElementById("dark-mode-button"));
+  enableFootnotes = sideFootnotes();
+
+  if (enableFootnotes) {
+    renderFootnotes();
+  }
+  renderCopyButton();
+}
+
 if (document.currentScript) {
-    enableFootnotes = document.currentScript.dataset.enableFootnotes == 'true'
+  enableFootnotes = sideFootnotes();
 }
 
-const init = () => {
-    new ThemeColorScheme(document.getElementById('dark-mode-button'))
-    if (enableFootnotes) {
-        renderFootnotes()
-    }
-    renderCopyButton()
-}
-
-window.addEventListener('load', () => {
-    setTimeout(function () {
-        init()
-    }, 0)
-})
+window.addEventListener("load", () => {
+  setTimeout(function () {
+    showFootnotes();
+  }, 0);
+});
